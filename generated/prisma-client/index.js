59be18d4071c5bfa6e94d9c89a8f105178b1cac3
thekanon <dlengjs12@naver.com>
2020-11-17 18:15:29 +0900

    데이터베이스 설계

---
+++ generated/prisma-client/index.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "User",
+    embedded: false
+  },
+  {
+    name: "Vote",
+    embedded: false
+  },
+  {
+    name: "UserVote",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://eu1.prisma.sh/dlengjs12-e14429/MBTI-statistical-project/dev`
+});
+exports.prisma = new exports.Prisma();
